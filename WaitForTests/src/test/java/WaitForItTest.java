783231df2d8b9100d5413ec631bc14cdb3c338b3
BaskaKlim <baska.klimekova@gmail.com>
2020-05-24 23:56:25 +0200

    setting up for simple test -> config for webdriver, WRONG

---
+++ WaitForTests/src/test/java/WaitForItTest.java
@@ -0,0 +1,31 @@
+import org.junit.*;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+public class WaitForItTest {
+    public WebDriver driver;
+
+    @Before
+
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver/");
+        driver = new ChromeDriver();
+    }
+
+    @After
+
+    public void tearDown() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+
+    public void testForInputText() {
+
+        driver.get("http://localhost:8888/waitforit.php");
+
+    }
+
+}
