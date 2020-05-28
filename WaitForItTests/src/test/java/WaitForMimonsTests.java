1623dc8443f1684fed55692f7c68da122289e103
BaskaKlim <baska.klimekova@gmail.com>
2020-05-28 23:13:55 +0200

    adding custom error message to WebDriverWait

--- WaitForItTests/src/test/java/WaitForMimonsTests.java
+++ WaitForItTests/src/test/java/WaitForMimonsTests.java
@@ -8,9 +8,7 @@ public class WaitForMimonsTests extends TestBase {
     @Before
     public void OpenUp() {
         driver.get("http://localhost:8888/minions.php");
-
     }
-
 
     @Test
 
@@ -28,5 +26,25 @@ public class WaitForMimonsTests extends TestBase {
         Assert.assertEquals(numberOfMimons,
                 driver.findElements(By.xpath("//div[@class='minions']//img")).size());
     }
+
+    @Test
+
+    public void itShouldShowErrorMessageIfNoEnoughMimonsWillShow(){
+        //actions
+        int numberOfMimons= 10;
+        driver.findElement(By.xpath("//div/input[@type='number']")).sendKeys("5");
+        driver.findElement(By.xpath("//div/button[contains(@class,'btn-warning')]")).click();
+
+        //wait for elements to be displayed  and declare error message for webdriverwait
+        new WebDriverWait(driver,5)
+                .withMessage("Timeout waiting for number of mimons to be 10")
+                .until(ExpectedConditions.numberOfElementsToBe((By.xpath("//div[@class='minions']//img")),numberOfMimons));
+
+        //validate the expected values and actions
+
+        Assert.assertEquals(numberOfMimons,
+                driver.findElements(By.xpath("//div[@class='minions']//img")).size());
+    }
+
 
 }
