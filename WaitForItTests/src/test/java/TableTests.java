67bff7a745e9e1b8c057813ab64a23b68dab83a7
BaskaKlim <baska.klimekova@gmail.com>
2020-05-28 23:50:18 +0200

    simple test that work with liest of elements - table rows

---
+++ WaitForItTests/src/test/java/TableTests.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import org.junit.*;
+import org.openqa.selenium.*;
+
+public class TableTests extends TestBase {
+
+    @Before
+    public void openUp() {
+        driver.get("http://localhost:8888/tabulka.php");
+    }
+
+    @Test
+
+    public void testHighLight() {
+        List<WebElement> rows = driver.findElements(By.xpath("//table/tbody/tr"));
+
+        for (WebElement row : rows) {
+            System.out.println(row.getText());
+        }
+    }
+}
