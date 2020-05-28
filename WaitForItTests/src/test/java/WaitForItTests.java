04862a1b42f81aec6bbb2826fd0b21cf945712a2
BaskaKlim <baska.klimekova@gmail.com>
2020-05-28 21:44:13 +0200

    created TestBase and update structure of Test package

--- WaitForItTests/src/test/java/WaitForItTests.java
+++ WaitForItTests/src/test/java/WaitForItTests.java
@@ -1,42 +1,25 @@
 import org.junit.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.support.ui.*;
 
-import static junit.framework.TestCase.*;
-
-public class WaitForItTests {
-
-    public WebDriver driver;
-    public StringBuffer verificationErrors = new StringBuffer();
+public class WaitForItTests extends TestBase{
 
 
     @Before
 
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-      
+    public void OpenUp() {
+        driver.get("http://localhost:8888/waitforit.php");
+
     }
-
-    @After
-
-    public void tearDown() {
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
-        driver.close();
-        driver.quit();
-    }
-
+    
     
     @Test
     public void waitForInputTest() throws InterruptedException {
-        driver.get("http://localhost:8888/waitforit.php");
+
         driver.findElement(By.id("startWaitForText")).click();
        new WebDriverWait(driver,5).until(ExpectedConditions.attributeToBe(By.id("waitForTextInput"),"value","dary !!!"));
         System.out.println(driver.findElement(By.id("waitForTextInput")).getAttribute("value"));
     }
+
+
 }
