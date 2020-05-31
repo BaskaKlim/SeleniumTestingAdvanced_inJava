94c1ba31b27df5067ec3847ece5155233a875bba
BaskaKlim <baska.klimekova@gmail.com>
2020-05-31 22:13:56 +0200

    test with multiple windows, using getWindowHandles and switchTo().window()

--- AdvancedSeleniumTests/src/test/java/InceptionTest.java
+++ AdvancedSeleniumTests/src/test/java/InceptionTest.java
@@ -10,16 +10,25 @@ public class InceptionTest extends TestBase {
     public void openUp() {
         driver.get("http://localhost:8888/inception.php");
         js = ((JavascriptExecutor) driver);
-      
 
     }
 
     @Test
 
-    public void testMultipleWindowsShowing(){
+    public void testMultipleWindowsShowing() {
         driver.findElement(By.id("letsGoDeeper")).click();
 
+        //wait for all windows I expected
         new WebDriverWait(driver, 5).until(ExpectedConditions.numberOfWindowsToBe(2));
+
+        //switch to new window to test webelement input
+        driver.getWindowHandles();
+        for (String window : driver.getWindowHandles()) {
+            driver.switchTo().window(window);
+
+        }
+        // test action
+        driver.findElement(By.xpath("//input[1]")).sendKeys("testovaci text");
 
     }
 }
