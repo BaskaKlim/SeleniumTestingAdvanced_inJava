8e78f3a1b65139b8d7471b14407af595e1c919bb
BaskaKlim <baska.klimekova@gmail.com>
2020-06-01 23:23:37 +0200

    read all emails from one excel sheet, using FileInputStream, XSSFWorkbook

--- AdvancedSeleniumTests/src/test/java/ExcelReader.java
+++ AdvancedSeleniumTests/src/test/java/ExcelReader.java
@@ -9,9 +9,23 @@ public class ExcelReader {
         FileInputStream fileInputStream = new FileInputStream(new File("src/test/resources/data/data.xlsx"));
         //read the file as an excel
         Workbook excelDataFile = new XSSFWorkbook(fileInputStream);
+
+        //TODO: printFirstRow
+
         //choose first sheet and first row
         Sheet sheet = excelDataFile.getSheetAt(0);
         Row row = sheet.getRow(0);
+        //print values
         System.out.println(row.getCell(1).getStringCellValue());
+
+        //TODO: printAllEmails
+        Sheet sheet1 = excelDataFile.getSheetAt(1);
+        for (Row row1 : sheet1) {
+            if (row1.getRowNum() == 0) {
+                continue;
+            }
+            System.out.println(row1.getCell(0).getStringCellValue());
+
+        }
     }
 }
