e6bc827d572f6bec8f0d6f62c3b8ec074546161d
BaskaKlim <baska.klimekova@gmail.com>
2020-06-05 23:05:03 +0200

    simple JUnit test, no webdriver selenium using

---
+++ AdvancedSeleniumTests/src/test/java/test/DummyTests.java
@@ -0,0 +1,32 @@
+package test;
+
+import org.junit.*;
+
+public class DummyTests {
+
+    @Test
+    public void testPrintA() {
+        System.out.println("A");
+    }
+
+    @Test
+    public void testPrintB() {
+        System.out.println("B");
+    }
+
+    @Test
+    public void testPrintC() {
+        System.out.println("C");
+    }
+
+    @Test
+    public void testPrintD() {
+        System.out.println("D");
+    }
+
+    @Test
+    public void testPrintE() {
+        System.out.println("E");
+    }
+
+}
