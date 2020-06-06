e00395f0c21f5e30ca59265e2d86063af254b6ca
BaskaKlim <baska.klimekova@gmail.com>
2020-06-06 22:09:12 +0200

    test with parameters, using anotation @Parameterzized.Parameters, preparing setup for test

---
+++ AdvancedSeleniumTests/src/test/java/test/ParameterTests.java
@@ -0,0 +1,52 @@
+package test;
+
+import java.util.*;
+import org.junit.*;
+import org.junit.runners.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+
+public class ParameterTests extends TestBase {
+
+    int number;
+    Boolean expectedPrimeStatus;
+
+    @Parameterized.Parameters
+    public static List<Integer> getData(){
+        return Arrays.asList(1,3,55,67,783,11);
+    }
+
+    @Before
+    public void openUp() {
+        driver.get("http://localhost:8888/primenumber.php");
+
+    }
+
+    @Test
+    public void primeNumberTestUsingParameters() {
+        WebElement numberInput = driver.findElement(By.xpath("//input[@type='number']"));
+        WebElement button = driver.findElement(By.cssSelector("button.btn"));
+
+
+        numberInput.clear();
+        numberInput.sendKeys(String.valueOf(number));
+        button.click();
+
+      
+        checkResult(expectedPrimeStatus);
+    }
+
+    /**private methods**/
+    
+    private void checkResult(boolean expectedPrimeStatus) {
+        if (expectedPrimeStatus == true) {
+            new WebDriverWait(driver, 5).until(ExpectedConditions.
+                    visibilityOfElementLocated(By.xpath("//div[text()='Optimus approves']")));
+        } else {
+            new WebDriverWait(driver, 5).until(ExpectedConditions.
+                    visibilityOfElementLocated(By.xpath("//div[text()='Optimus is sad']")));
+
+        }
+    }
+
+}
