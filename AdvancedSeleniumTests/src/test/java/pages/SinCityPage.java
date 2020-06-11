85809b4b1e80202ef806ba7a5a6152d3fbf836a6
BaskaKlim <baska.klimekova@gmail.com>
2020-06-11 23:44:49 +0200

    page method confessSin

--- AdvancedSeleniumTests/src/test/java/pages/SinCityPage.java
+++ AdvancedSeleniumTests/src/test/java/pages/SinCityPage.java
@@ -10,22 +10,31 @@ import models.*;
 public class SinCityPage {
 
     private WebDriver driver;
+
     @FindBy(name = "title")
     private WebElement titleInput;
 
+    @FindBy(name = "author")
+    private WebElement authorInput;
+
+    @FindBy(name = "message")
+    private WebElement messageInput;
+
+
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement confessButton;
+
     //konstruktor
-    public SinCityPage(){
-    driver = WebDriverSingleton.getWebDriverInstance();
-    PageFactory.initElements(driver,this);
+    public SinCityPage() {
+        driver = WebDriverSingleton.getWebDriverInstance();
+        PageFactory.initElements(driver, this);
     }
 
-
-   public  void fillSinInformation(Sin sin) {
+    public void fillSinInformation(Sin sin) {
         titleInput.sendKeys(sin.getTitle());
-        driver.findElement(By.xpath("//input[@name='author']")).sendKeys(sin.getAuthor());
-        driver.findElement(By.name("message")).sendKeys(sin.getMessage());
+        authorInput.sendKeys(sin.getAuthor());
+        messageInput.sendKeys(sin.getMessage());
     }
-
 
     public void markTag(List<SinTag> tags) throws InterruptedException {
 
@@ -34,7 +43,8 @@ public class SinCityPage {
             Thread.sleep(5000);
         }
     }
-
-
+    public void confessSin(){
+        confessButton.click();
+    }
 
 }
