6de6730ce2f512265c8d142c6108883cdadbf9f3
BaskaKlim <baska.klimekova@gmail.com>
2020-06-11 21:20:38 +0200

    refactoring: using stream instead of iterations and conditions

--- AdvancedSeleniumTests/src/test/java/test/StreamTests.java
+++ AdvancedSeleniumTests/src/test/java/test/StreamTests.java
@@ -5,7 +5,6 @@ import org.junit.*;
 import org.openqa.selenium.*;
 import base.*;
 
-
 public class StreamTests extends TestBase {
 
     @Before
@@ -19,20 +18,16 @@ public class StreamTests extends TestBase {
         List<WebElement> rows = getDriver().findElements(By.xpath("//table/tbody/tr"));
         List<String> vendelinsSurnames = new ArrayList<>();
 
-        for (WebElement row : rows) {
-            if("Vendelin".equals(getFirstName(row))) {
-               vendelinsSurnames.add(getSurname(row));
-            }
-        }
-        System.out.println(vendelinsSurnames);
+        rows.stream().filter(row -> getFirstName(row).equals("Vendelin"))
+                .forEach(row -> System.out.println(getSurname(row)));
     }
 
     //funkcia na vybranie stlpca s menom, s parametrom webelement riadok
-    private String getFirstName(WebElement element){
-          return element.findElement(By.xpath("./td[2]")).getText();
+    private String getFirstName(WebElement element) {
+        return element.findElement(By.xpath("./td[2]")).getText();
     }
 
-    private String getSurname(WebElement element){
+    private String getSurname(WebElement element) {
         return element.findElement(By.xpath("./td[3]")).getText();
     }
 
