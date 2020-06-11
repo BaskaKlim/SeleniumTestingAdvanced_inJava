82665895409c28f67d49e6b56e0bda9f6fdccd4d
BaskaKlim <baska.klimekova@gmail.com>
2020-06-11 20:07:11 +0200

    Using stream: preparing simple test for get row with First Names from tables

---
+++ AdvancedSeleniumTests/src/test/java/test/StreamTests.java
@@ -0,0 +1,32 @@
+package test;
+
+import java.util.*;
+import org.junit.*;
+import org.openqa.selenium.*;
+import base.*;
+
+
+public class StreamTests extends TestBase {
+
+    @Before
+    public void openUp() {
+        getDriver().get(BASE_URL + "tabulka.php");
+    }
+
+    @Test
+
+    public void testHighLight() {
+        List<WebElement> rows = getDriver().findElements(By.xpath("//table/tbody/tr"));
+
+        for (WebElement row : rows) {
+            System.out.println(getFirstName(row));
+        }
+    }
+
+    //funkcia na vybranie stlpca s menom, s parametrom webelement riadok
+    private String getFirstName(WebElement element){
+          return element.findElement(By.xpath("./td[2]")).getText();
+
+    }
+
+}
